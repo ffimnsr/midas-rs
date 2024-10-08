b2276c63a52263b38514eee25dc40a665e9e795a
Edward Fitz Abucay <ffimnsr@gmail.com>
2024-10-08 14:31:52 +0800

    🍕 Add makefile toml and githooks
    Signed-off-by: Edward Fitz Abucay <ffimnsr@gmail.com>

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-if ! command -v docker; then
-    podman build -f Containerfile.distroless -t ghcr.io/ffimnsr/midas-rs:latest .
-else
-    docker build -f Containerfile.distroless -t ghcr.io/ffimnsr/midas-rs:latest .
-fi
